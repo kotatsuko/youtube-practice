2eaeabad01c9ec871b92c9abbb58404fa464a075
kotatsuko <107920100+kotatsuko@users.noreply.github.com>
2022-07-24 01:52:26 +0000

    投稿検索

---
+++ app/controllers/youtubes_controller.rb
@@ -0,0 +1,24 @@
+class YoutubesController < ApplicationController
+
+
+  def find_videos(keyword, after: 1.months.ago, before: Time.now)
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = "AIzaSyCncaj1O4oabCnZxHi6LR5b7xYu-s6QcgA"
+
+    next_page_token = nil
+    opt = {
+      q: keyword,
+      type: 'video',
+      max_results: 3,
+      order: :date,
+      page_token: next_page_token,
+      published_after: after.iso8601,
+      published_before: before.iso8601
+    }
+    service.list_searches(:snippet, opt)
+  end
+
+  def index
+    @youtube_data = find_videos('ホロライブ')
+  end
+end
